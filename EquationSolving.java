f7bc7d181cf89d7ffd01a49c0f7c830874a88c30
Mingix-PT <mingixpt@gmail.com>
2023-10-12 13:22:55 +0700

    Complete assignment 2.2.6

--- EquationSolving.java
+++ EquationSolving.java
@@ -20,20 +20,17 @@ public class EquationSolving {
             a = input.nextDouble();
             System.out.printf("Please input b: ");
             b = input.nextDouble();
-            if (a==0) {
-                if (b==0) {
+            if (a == 0) {
+                if (b == 0) {
                     System.out.println("The equation has infinite solutions");
-                }
-                else {
+                } else {
                     System.out.println("The equation has no solution");
                 }
-            }
-            else {
-                x = -b/a;
+            } else {
+                x = -b / a;
                 System.out.println("The equation has one solution: x = " + x);
             }
-        }
-        else if (option == 2) {
+        } else if (option == 2) {
             double a11, a12, a21, a22, b1, b2, x1, x2;
             System.out.printf("Please input a11: ");
             a11 = input.nextDouble();
@@ -47,21 +44,18 @@ public class EquationSolving {
             a22 = input.nextDouble();
             System.out.printf("Please input b2: ");
             b2 = input.nextDouble();
-            if (a11*a22 - a12*a21 == 0) {
-                if (b1*a22 - b2*a12 == 0 && b2*a11 - b1*a21 == 0) {
+            if (a11 * a22 - a12 * a21 == 0) {
+                if (b1 * a22 - b2 * a12 == 0 && b2 * a11 - b1 * a21 == 0) {
                     System.out.println("The equation has infinite solutions");
-                }
-                else {
+                } else {
                     System.out.println("The equation has no solution");
                 }
-            }
-            else {
-                x1 = (b1*a22 - b2*a12)/(a11*a22 - a12*a21);
-                x2 = (b2*a11 - b1*a21)/(a11*a22 - a12*a21);
+            } else {
+                x1 = (b1 * a22 - b2 * a12) / (a11 * a22 - a12 * a21);
+                x2 = (b2 * a11 - b1 * a21) / (a11 * a22 - a12 * a21);
                 System.out.println("The equation has one solution: x1 = " + x1 + ", x2 = " + x2);
             }
-        }
-        else {
+        } else {
             double a, b, c, delta, x1, x2;
             System.out.printf("Please input a: ");
             a = input.nextDouble();
@@ -69,32 +63,27 @@ public class EquationSolving {
             b = input.nextDouble();
             System.out.printf("Please input c: ");
             c = input.nextDouble();
-            if (a==0) {
-                if (b==0) {
-                    if (c==0) {
+            if (a == 0) {
+                if (b == 0) {
+                    if (c == 0) {
                         System.out.println("The equation has infinite solutions");
-                    }
-                    else {
+                    } else {
                         System.out.println("The equation has no solution");
                     }
-                }
-                else {
-                    x1 = -c/b;
+                } else {
+                    x1 = -c / b;
                     System.out.println("The equation has one solution: x = " + x1);
                 }
-            }
-            else {
-                delta = b*b - 4*a*c;
+            } else {
+                delta = b * b - 4 * a * c;
                 if (delta < 0) {
                     System.out.println("The equation has no solution");
-                }
-                else if (delta == 0) {
-                    x1 = -b/(2*a);
+                } else if (delta == 0) {
+                    x1 = -b / (2 * a);
                     System.out.println("The equation has one solution: x = " + x1);
-                }
-                else {
-                    x1 = (-b + Math.sqrt(delta))/(2*a);
-                    x2 = (-b - Math.sqrt(delta))/(2*a);
+                } else {
+                    x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                    x2 = (-b - Math.sqrt(delta)) / (2 * a);
                     System.out.println("The equation has two solutions: x1 = " + x1 + ", x2 = " + x2);
                 }
             }
