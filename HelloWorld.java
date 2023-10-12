03e3a95a76341c4be87a8e075a41a134b38d02ab
Mingix-PT <mingixpt@gmail.com>
2023-10-12 09:38:26 +0700

    Complete assignment 2.2.1

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+//Exarple 1: HelloWorld.java
+//Text-printing program
+public class HelloWorld {
+    //main method begins execution of Java application
+    public static void main(String[] args) {
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t World!");
+    }//end method main
+}//end class HelloWorld
